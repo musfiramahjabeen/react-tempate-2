4f0597b2c45c683868c1daf57d590062a65a81d8
musfiramahjabeen <musfiramahjabeen54@gmail.com>
2025-03-07 08:35:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,146 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container_1">
+
+  <div className="logo">
+    <h1>FLOMIC</h1>
+    <p>logistics PVT. LTD</p>
+  </div>
+
+  <div className="menu">
+
+    <button>Home</button>
+
+    <select id="options" name="options">
+      <option id="options" name="options">PAGES</option>
+    </select>
+
+    <select id="options" name="options">
+      <option id="options" name="options">DROPDOWN</option>
+    </select>
+
+    <a href="#">Link</a>
+    <a href="#">link</a>
+
+  </div>
+  </div>
+
+  <div className="container_2">
+  <div className="seg">
+    <h3>Lorem ipsum dolor sit Lorem ipsum dolor.</h3><br/>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisici Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quod eum consequatur illo veritatis dolorem, laboriosam.</p>
+    <button>READ MORE </button>
+    </div>
+
+  <div className="seg">
+    <h3>Lorem ipsum dolor sit Lorem ipsum dolor.</h3><br/>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisici Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quod eum consequatur illo veritatis dolorem, laboriosam.</p>
+    <button>READ MORE </button>
+    </div>
+
+  <div className="seg">
+    <h3>Lorem ipsum dolor sit Lorem ipsum dolor.</h3><br/>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisici Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quod eum consequatur illo veritatis dolorem, laboriosam.</p>
+    <button>READ MORE </button>
+    </div>
+
+  </div>
+  <div className="container_3">
+  <div className="seg1">
+      <h2>Lorem ipsum dolor consectetur.</h2>
+      <div className="box1">
+            <img src="coffee3.jpg" alt="img" />
+            <div className="overlay_txt">
+              <h3>Lorem ipsum, dolor sit amet</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis commodi excepturi </p>
+              <button>READ MORE</button>
+            </div>
+      </div>
+
+  </div>
+      <div className="seg2">
+        
+          <h2>Lorem ipsum dolor consectetur.</h2>
+          <div className="box2">
+              <img src="coffee2.jpg" alt="img"></img>
+
+              <div className='content'>
+                <p>Lorem ipsum dolor sit Lorem ipsum dolor.dolor sit amet consectetur, adipisici Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <button>READ MORE</button>
+              </div>
+                
+          </div> 
+
+          <div className="box2">
+              <img src="coffee2.jpg" alt="img"></img>
+
+              <div className='content'>
+                  <p>Lorem ipsum dolor sit Lorem ipsum dolor.dolor sit amet consectetur, adipisici Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                  <button>READ MORE</button>
+              </div>
+
+          </div> 
+
+          <div className="box2">
+              <img src="coffee2.jpg" alt="img"></img>
+
+              <div className='content'>
+                  <p>Lorem ipsum dolor sit Lorem ipsum dolor.dolor sit amet consectetur, adipisici Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                  <button>READ MORE</button>
+              </div>
+
+          </div> 
+
+      </div>
+
+  </div>
+  <div className='container_4'>
+  <h2>Lorem ipsum dolor sit Distinctio possimus odio dolorem.</h2>
+  <p> Dolorem laudantium doloribus exercitationem architecto dignissimos qui delectus, odit repellendus tempore maxime ex. Totam, perspiciatis dicta!</p>
+  <div className='seg3'>
+        <div className="box3">
+              <img src="coffee2.jpg" alt="img"></img>
+              <h2>Dolorem laudantium doloribus exercitationem architecto dignissimos qui delectus, odit repellendus<span><br></br><button>READ MORE</button></span></h2>      
+        </div>
+
+
+        <div className="box3">
+              <img src="coffee2.jpg" alt="img"></img>
+              <h2>Dolorem laudantium doloribus exercitationem architecto dignissimos qui delectus, odit repellendus<span><br></br><button>READ MORE</button></span></h2>
+        </div>
+  </div>
+  </div>
+  <div className="container_5">
+  <div className="seg4">
+        <h3>Lorem IPSUM DOLOR</h3><hr></hr>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, in quas. Sed enim nemo ullam, commodi quo ut maxime pariatur</p>
+        <p>Street name & number , town , postcode/zip</p>
+        <p>+00 (123) 456 7890 </p>
+        <p>info@domain.com</p>
+  </div>
+
+  <div className="seg4">
+        <h3>QUICK LINKS</h3><hr></hr>
+        <a>Home Page</a>
+        <a>BLOG</a>
+        <a>gallery</a>
+        <a>PORTFOLIO</a>
+        <a>Contact Us</a>
+    
+  </div>
+  <div className="seg4">
+    <h2>KEEP IN TOUCH</h2><hr></hr>
+    <input type='text' name="email" id ="email" placeholder='Email'></input><br></br>
+    <button>SUBMIT</button>
+  </div>
+  </div>
+
+    </>
+  )
+}
+
+export default App
